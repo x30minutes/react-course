a3ee98f7499cb3efb165b0ca35766dab8ff9e6ae
Hubert Kulicki <hubertkulicki@gmail.com>
2022-07-22 10:16:00 +0200

    upload cart project

---
+++ 03.basic-projects/14-cart/src/reducer.js
@@ -0,0 +1,79 @@
+import CartItem from './CartItem';
+
+const reducer = (state, action) => {
+  if (action.type === 'CLEAR_CART') {
+    return { ...state, cart: [] };
+  }
+  if (action.type === 'REMOVE_ITEM') {
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload.id),
+    };
+  }
+  if (action.type === 'INCREASE') {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload.id) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === 'DECREASE') {
+    let tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload.id) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === 'GET_TOTAL') {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, total, amount };
+  }
+  if (action.type === 'LOADING') {
+    return { ...state, loading: true };
+  }
+  if (action.type === 'DISPLAY_ITEMS') {
+    return { ...state, cart: action.payload.cart, loading: false };
+  }
+  if (action.type === 'TOGGLE_AMOUNT') {
+    let tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload.id) {
+          if (action.payload.type === 'increase') {
+            return { ...item, amount: item.amount + 1 };
+          }
+          if (action.payload.type === 'decrease') {
+            return { ...item, amount: item.amount - 1 };
+          }
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
+
+    return { ...state, cart: tempCart };
+  }
+
+  throw new Error('no matching action type');
+};
+
+export default reducer;
