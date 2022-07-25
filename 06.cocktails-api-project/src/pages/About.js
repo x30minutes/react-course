45a0198a72cfe2dd7c50f5ebd46e52e7af4c1160
Hubert Kulicki <hubertkulicki@gmail.com>
2022-07-25 16:36:59 +0200

    upload cocktail project

---
+++ 06.cocktails-api-project/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        repudiandae rem eum quod sint praesentium alias eligendi culpa! Omnis,
+        molestias dolorem excepturi inventore magnam quos vitae debitis velit ut
+        saepe maiores tempora, consequuntur sunt reprehenderit in quasi quas!
+        Harum, assumenda.
+      </p>
+    </section>
+  );
+};
+
+export default About;
